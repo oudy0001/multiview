a4ec0696b3e62d8873ae8d11f3c8d36e0cabbf12
Christopher <chrisoudyk@Christophers-MacBook-Pro.local>
2015-11-02 15:25:39 -0500

    -somehow all my content is wrapped inside the button. changed button from self-closing to better xml, solved problem (30 seconds after last sync) -can’t find ngRoute, trying to install it with bower, but ionic won’t use the new version of angular and regular ngRoute requires the new version

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,8 @@
-angular.module('multiview').controller('ionSideMenus', function(){
-    
+angular.module('multiview')
+.controller('ionSideMenus', function($scope, $ionicSideMenueDelegate){
+    $scope.toggleMenue = function(){
+        alert('hi');
+        $ionicSideMenueDelegate.toggleLeft();
+    }
     
 });
