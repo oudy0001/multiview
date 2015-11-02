bb412bb61baba882086e0eab4a18939bb8b477c0
Christopher <chrisoudyk@Christophers-MacBook-Pro.local>
2015-11-02 14:52:22 -0500

    got distracted from the title issue instead: -looked at assignment, realized I needed routing -ngRoutes doesn’t want to be loaded, removing injector solved the problem -confused about the “no CDN references”, looking into downloading angular and requiring the file in bower.

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,9 @@
 angular.module('multiview')
+//.config(function($routeProvider) {
+//    $routeProvider
+//    .when('/', {
+//        templateUrl: 'templates/lists.html',
+//        controller: 'ListCtrl'
+//    })
+//    .otherwise({redirectTo: '/'})
+//})
