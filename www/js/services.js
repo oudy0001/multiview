b01d7997b65e6fab17b7b05427c4741aa474f684
Christopher <chrisoudyk@Christophers-MacBook-Pro.local>
2015-11-02 15:42:29 -0500

    added android,
    -added android
    -installed angular-router 1.4.3 but ionic can’t find it, digging
    through ionic.bundle.js to try and get a hint

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,9 @@
 angular.module('multiview')
-.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'templates/lists.html',
-        controller: 'ionSideMenus'
-    })
-    .otherwise({redirectTo: '/'})
-})
+//.config(function($routeProvider) {
+//    $routeProvider
+//    .when('/', {
+//        templateUrl: 'templates/lists.html',
+//        controller: 'ionSideMenus'
+//    })
+//    .otherwise({redirectTo: '/'})
+//})
